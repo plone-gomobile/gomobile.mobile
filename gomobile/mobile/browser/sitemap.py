faa08d45eb27133073d608c41be3cbddbfe9b2d7
mikko@redinnovation.com <mikko@redinnovation.com@985c90ce-6bbf-11de-a1e0-47924000d34a>
2010-03-24 12:53:09 +0000

    Added mobile sitemap support and tests

---
+++ gomobile/mobile/browser/sitemap.py
@@ -0,0 +1,147 @@
+"""
+
+    Mobile sitemap support.
+    
+    http://www.google.com/support/webmasters/bin/answer.py?hl=en&answer=34648
+    
+    http://mfabrik.com
+
+"""
+
+__license__ = "GPL 2"
+__copyright__ = "2010 mFabrik Research Oy"
+__author__ = "Mikko Ohtamaa <mikko@mfabrik.com>"
+__docformat__ = "epytext"
+
+
+from gzip import GzipFile
+from cStringIO import StringIO
+
+from Products.Five import BrowserView
+from zope.publisher.interfaces import NotFound
+from Products.CMFCore.utils import getToolByName
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+from plone.app.layout.sitemap import sitemap as base
+from plone.memoize import ram
+
+def _render_cachekey(fun, self):
+    # Cache by filename
+    mtool = getToolByName(self.context, 'portal_membership')
+    if not mtool.isAnonymousUser():
+        raise ram.DontCache
+
+    url_tool = getToolByName(self.context, 'portal_url')
+    catalog = getToolByName(self.context, 'portal_catalog')
+    counter = catalog.getCounter()
+    return '%s/%s/%s' % (url_tool(), self.get_filename(), counter)
+
+class LanguageAwareMobileSitemap(base.SiteMapView):
+    """
+    Allow generation of language specific sitemaps.
+    
+    Example Google Webmaster tools links:
+    
+    * http://yoursite/@@mobile_sitemap?language=fi&mode=mobile
+    
+    * http://yoursite/@@mobile_sitemap?language=fi&mode=mobile
+    
+    * http://yoursite/@@mobile_sitemap?language=ALL&mode=web
+    
+    Example test runs (does not GZip content):
+    
+    * http://yoursite/@@mobile_sitemap?language=fi&mode=mobile&test
+    """
+    
+    template = ViewPageTemplateFile("templates/mobile-sitemap.pt")
+
+    def getMode(self):
+        """
+        Should we pages as mobile.
+        
+        @return: "web" or "mobile"
+        """
+        mode = self.request.form.get("mode", "mobile")
+        return mode
+
+    def getLanguage(self):
+        """
+        Read language from the request.
+        
+        @return: Two letter language code or "ALL"
+        """
+        lang = self.request.form.get("language", None)
+
+        # Enforce we do not leak wrong language items
+        if lang == None:
+            raise RuntimeError("Please give two letter language code or 'ALL' as 'language' GET parameter")
+
+        # Some basic validation
+        assert (len(lang)==2 or len(lang) == 3), "Bad input language"
+
+        return lang
+
+    def get_filename(self):
+        """
+        Calculate GZipped filename.
+        """
+        #print "Lang:" + str(self.getLanguage())
+        #print "Mode:" + str(self.getMode())
+        return self.getLanguage() + "-" + self.getMode() + "-sitemap.xml.gz"
+
+    @ram.cache(_render_cachekey)
+    def generate(self):
+        """Generates the Gzipped sitemap."""
+        xml = self.template()
+        fp = StringIO()
+        gzip = GzipFile(self.get_filename(), 'w', 9, fp)
+        gzip.write(xml)
+        gzip.close()
+        data = fp.getvalue()
+        fp.close()
+        return data
+
+    def uncompressed(self):
+        """
+        Return uncompressed data
+        """
+        return self.template()
+
+    def objects(self):
+        """Returns the data to create the sitemap."""
+        catalog = getToolByName(self.context, 'portal_catalog')
+        for item in catalog.searchResults({'Language': self.getLanguage()}):
+            yield {
+                'loc': item.getURL(),
+                'lastmod': item.modified.ISO8601(),
+                'changefreq': 'daily',
+                #'prioriy': 0.5, # 0.0 to 1.0
+            }
+
+
+    def is_mobile(self):
+        """
+        Expose to template whether we should insert <mobile> tags.
+        """
+        return self.getMode() == "mobile"
+
+    def __call__(self):
+        """Checks if the sitemap feature is enable and returns it."""
+
+        # Sitemap must be enabled in site settings
+        sp = getToolByName(self.context, 'portal_properties').site_properties
+        if not sp.enable_sitemap:
+            raise NotFound(self.context, self.get_filename(), self.request)
+
+
+        
+        
+        if self.request.get("uncompressed", None) != None:
+            self.request.response.setHeader('Content-Type','text/xml')            
+            return self.uncompressed()
+        else:
+            self.request.response.setHeader('Content-Type', 'application/octet-stream')
+            return self.generate()
+
+
+
---
+++ gomobile/mobile/tests/test_sitemap.py
@@ -0,0 +1,76 @@
+__license__ = "GPL 2"
+__copyright__ = "2010 mFabrik Research Oy"
+
+
+import unittest
+from urllib2 import HTTPError
+
+from zope.component import getUtility, queryUtility, queryMultiAdapter
+
+from Products.CMFCore.utils import getToolByName
+
+from gomobile.mobile.tests.base import BaseFunctionalTestCase
+from gomobile.mobile.interfaces import IUserAgentSniffer
+
+from gomobile.mobile.browser.views import FolderListingView
+
+class TestSitemap(BaseFunctionalTestCase):
+    """ Test mobile sitemap functions.
+    
+    TODO: this do basic code coverage tests and little output validation. 
+    """
+    
+    def afterSetUp(self):
+        
+        BaseFunctionalTestCase.afterSetUp(self)
+        
+        # Sitemap must be enabled from the settings to access the view
+        self.portal.portal_properties.site_properties.enable_sitemap = True
+
+    
+    def test_mobile_sitemap(self):
+        """ Check that accuracy of UA match is delivered to us correctly. """
+        
+        url = self.portal.absolute_url() + "/@@mobile_sitemap?mode=mobile&language=en&uncompressed"
+        self.browser.open(url)
+        print self.browser.contents
+        
+        self.assertTrue('xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0"' in self.browser.contents)        
+        self.assertTrue("<mobile:mobile/>" in self.browser.contents)
+
+    def test_mobile_sitemap_web_mode(self):
+        """ Check that accuracy of UA match is delivered to us correctly. """
+        
+        url = self.portal.absolute_url() + "/@@mobile_sitemap?language=en&mode=web&uncompressed"
+        self.browser.open(url)
+        
+        self.assertFalse("mobile:mobile" in self.browser.contents)
+        
+    def test_mobile_sitemap_all_languages(self):
+        """ Check that accuracy of UA match is delivered to us correctly. """
+        
+        url = self.portal.absolute_url() + "/@@mobile_sitemap?mode=mobile&language=ALL"
+        self.browser.open(url)
+
+    def test_no_language(self):
+        """ Check that language parameter is needed and nothing is executed unless it is given. """
+        
+        from urllib2 import HTTPError
+        try:
+            self.browser.handleErrors = True # Don't get HTTP 500 pages
+            url = self.portal.absolute_url() + "/@@mobile_sitemap?mode=mobile"
+            self.browser.open(url)
+            # should cause HTTPError: HTTP Error 500: Internal Server Error
+            raise AssertionError("Should be never reached")
+        except HTTPError, e:
+            pass        
+
+    def test_compressed(self):        
+        url = self.portal.absolute_url() + "/@@mobile_sitemap?mode=mobile&language=en"
+        self.browser.open(url)        
+        self.assertEqual(self.browser.headers["Content-type"], 'application/octet-stream')  
+
+def test_suite():
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(TestSitemap))
+    return suite
