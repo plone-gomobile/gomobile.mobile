8cb35a5d5713516007df1db8f2f288951a879d68
mikko@redinnovation.com <mikko@redinnovation.com@985c90ce-6bbf-11de-a1e0-47924000d34a>
2010-08-31 13:52:21 +0000

    Updated Javascript based tracking script

--- gomobile/mobile/trackers/google.py
+++ gomobile/mobile/trackers/google.py
@@ -1,9 +1,8 @@
 """
 
-    AdMob tracker - register at www.admob.com
-
-
-    The orignal code courtesy of http://www.djangosnippets.org/snippets/883/
+    Google Analytics tracking backends.
+    
+    Both Javascript based and image based.
 
 """
 
@@ -80,6 +79,16 @@ class GoogleAnalyticsWebTracker(object):
 
 # Async. Urchin tracking code
 URCHIN_CODE="""
-<script></script>
-<script>%s</script>
-"""
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', '%s']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>"""
