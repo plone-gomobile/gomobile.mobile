ee42c70bc64c3fde9695901ad5fda68e53f43269
maurits.vanrees <maurits.vanrees@985c90ce-6bbf-11de-a1e0-47924000d34a>
2010-12-10 18:00:50 +0000

    PPP (PeP8/Pyflakes) on redirectory.py

--- gomobile/mobile/redirector.py
+++ gomobile/mobile/redirector.py
@@ -14,13 +14,13 @@ from Acquisition import aq_inner
 
 from datetime import datetime
 
-from Products.Five.browser import BrowserView
-
 from zope.app.component.hooks import getSite
 from zope.component import getUtility, queryMultiAdapter, getMultiAdapter
 from OFS.interfaces import IApplication
 
-from gomobile.mobile.interfaces import IMobileRequestDiscriminator, MobileRequestType, IUserAgentSniffer, IMobileSiteLocationManager, IMobileLayer, IGoMobileInstalled
+from gomobile.mobile.interfaces import (
+    IMobileRequestDiscriminator, MobileRequestType, IUserAgentSniffer,
+    IMobileSiteLocationManager)
 
 from mobile.sniffer.utilities import get_user_agent
 
@@ -33,7 +33,8 @@ class Redirector(object):
     If a request arrives with GET parameter force_web then set a cookie which
     makes web session persistent.
 
-    Otherwise redirect the user to the mobile site if mobile user agent is sniffed.
+    Otherwise redirect the user to the mobile site if mobile user
+    agent is sniffed.
     """
 
     # If cookie is set and contains value "web" then stay on the web site
@@ -57,9 +58,9 @@ class Redirector(object):
 
         return False
 
-
     def isCookiedWeb(self):
-        """ Check if cookie is set so that mobile browser is forced to stay on the web site.
+        """ Check if cookie is set so that mobile browser is forced to
+        stay on the web site.
         """
         return self.request.cookies.get(Redirector.COOKIE_NAME) == "web"
 
@@ -76,10 +77,9 @@ class Redirector(object):
     def getRealContext(self):
         """ Get contentish context for the context.
 
-        Since we are a traversing hook, the context object might be bit heterogenous-
+        Since we are a traversing hook, the context object might be
+        bit heterogenous-
         """
-
-
 
         context = aq_inner(self.context)
         return context
@@ -90,7 +90,7 @@ class Redirector(object):
         if hasattr(self.context, "aq_parent"):
             return self.context.aq_parent
         else:
-            return None # This context lacks acquisition chain, happens
+            return None  # This context lacks acquisition chain, happens
 
     def forceWeb(self):
         """
@@ -99,25 +99,27 @@ class Redirector(object):
         response = self.request.response
         response.setCookie(Redirector.COOKIE_NAME, "web", path="/")
 
-    def redirect_url(self, url, query_string, media_type=MobileRequestType.MOBILE):
+    def redirect_url(self, url, query_string,
+                     media_type=MobileRequestType.MOBILE):
         """ HTTP redirect to a mobile site matching certain URL.
 
         @param url: Base URL to rewrite
 
-        @param query_string: Incoming query string on the orignal request (for analytics preservation etc.)
+        @param query_string: Incoming query string on the orignal
+            request (for analytics preservation etc.)
 
         @param media_type: Target media type. "www" or "mobile"
 
         """
         context = self.getRealContext()
 
-        location_manager = getMultiAdapter((context, self.request), IMobileSiteLocationManager)
+        location_manager = getMultiAdapter(
+            (context, self.request), IMobileSiteLocationManager)
 
         new_url = location_manager.rewriteURL(url, media_type)
 
         if query_string != "":
             new_url += "?" + query_string
-
 
         if media_type == MobileRequestType.WEB:
             # Add magical HTTP GET parameter which
@@ -133,7 +135,8 @@ class Redirector(object):
         self.request.response.redirect(new_url)
 
     def redirect(self, media_type=MobileRequestType.MOBILE):
-        """ Redirects to the mobile site staying on the page pointed by the current HTTP request.
+        """ Redirects to the mobile site staying on the page pointed
+        by the current HTTP request.
 
         Rewrites the current HTTP response.
 
@@ -160,23 +163,23 @@ class Redirector(object):
         #if not IGoMobileInstalled.providedBy(self.request):
         #    return False
 
-
         # This is needed for templates without views
         context = self.getRealContext()
         if context is None:
             return False
 
         try:
-            location_manager = getMultiAdapter((context, self.request), IMobileSiteLocationManager)
+            getMultiAdapter((context, self.request),
+                            IMobileSiteLocationManager)
         except:
-            # XXX: gomobile.mobile is not installed
-            # Should check for IGoMobileInstalled interface, but support is not yet enabled
-            # - see intercept()
+            # XXX: gomobile.mobile is not installed.  Should check for
+            # IGoMobileInstalled interface, but support is not yet
+            # enabled - see intercept()
             return False
 
-
         if IApplication.providedBy(context):
-            # Do not intercept requests going to the Zope management interface root (one level above Plone sites)
+            # Do not intercept requests going to the Zope management
+            # interface root (one level above Plone sites)
             return False
 
         discriminator = getUtility(IMobileRequestDiscriminator)
@@ -189,7 +192,6 @@ class Redirector(object):
         #print "Sniffed: " + str(self.isSniffedMobile())
         #print "Is web:" + str(is_web)
         #print "ModeS:" + str(modes)
-
 
         if self.isSniffedMobile() and is_web:
 
@@ -208,7 +210,6 @@ class Redirector(object):
             return False
 
 
-
 class LoggingRedirector(Redirector):
     """
     A redirector which writes a log entry for every redirect, so that the site
@@ -221,13 +222,14 @@ class LoggingRedirector(Redirector):
 
     def redirect(self):
         self.addLogEntry()
-        self.buffer.flush() # Don't hide data
+        self.buffer.flush()  # Don't hide data
         return Redirector.redirect(self)
 
     def createLogger(self):
         """
         """
-        self.buffer = open("/tmp/redirect.log", "at", 0) # text + append only + no buffering
+        # text + append only + no buffering
+        self.buffer = open("/tmp/redirect.log", "at", 0)
 
     def addLogEntry(self):
         """
