eb422c0fef8636937329b30c5a05efcfcdfc516f
mikko@redinnovation.com <mikko@redinnovation.com@985c90ce-6bbf-11de-a1e0-47924000d34a>
2011-01-31 13:43:14 +0000

    Added output of cleaning up

--- gomobile/mobile/Extensions/Install.py
+++ gomobile/mobile/Extensions/Install.py
@@ -5,18 +5,24 @@
     
 """
 
+
+from StringIO import StringIO
+
 from Products.CMFCore.utils import getToolByName
 
 from gomobile.mobile.setuphandlers import clean_up_content_annotations
 
 def uninstall(portal, reinstall=False):
 
+    output = StringIO()
     if not reinstall:
 
         # normal uninstall
         setup_tool = getToolByName(portal, 'portal_setup')
         setup_tool.runAllImportStepsFromProfile('profile-gomobile.mobile:uninstall')
       
-        clean_up_content_annotations(portal, ["mobile"])
-
-        return "Ran all uninstall steps."
+        report = clean_up_content_annotations(portal, ["mobile"])
+        print >> output, report
+        
+        print >> output, "Ran all uninstall steps."
+        return output
